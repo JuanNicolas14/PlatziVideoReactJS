c6c63e8fa5ba356baf8a983fd8291b9f6aea0c73
Juan Nicolas Cardenas <juan.cardenasc@outlook.com>
2021-02-09 18:01:39 -0500

    Proyecto basico de react finalizado

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import '../assets/styles/components/Header.scss';
+import logo from '../assets/static/logo-platzi-video-BW2.png';
+import userIcon from '../assets/static/user-icon.png';
+
+const Header = () => (
+  <header className="header">
+    <img src={logo} alt="Platzi Video" className="header__img"/>
+    <div className="header__menu">
+      <div className="header__menu--profile">
+        <img src={userIcon} alt="Icono usuario"/>
+        <p>Perfil</p>
+      </div>
+      <ul>
+        <li><a href="/">Cuenta</a></li>
+        <li><a href="/">Cerrar Sesion</a></li>
+      </ul>
+    </div>
+  </header>
+);
+
+export default Header;
